ac87bdf33a32dd057ade8e0037773e154ba5d6a4
chanminmaxscaler <jcm04@naver.com>
2022-10-04 15:11:37 +0900

    수업 커밋 단축키로 수정해보기

--- src/backend1004/CurrencyTest.java
+++ src/backend1004/CurrencyTest.java
@@ -1,7 +1,5 @@
 package backend1004;
 
-import java.util.Scanner;
-
 public class CurrencyTest {
     public static void main(String[] args) {
         int money = 25000;
@@ -15,21 +13,21 @@ public class CurrencyTest {
         int curr50 = 50;
         int curr10 = 10;
 
-        System.out.printf("50000원 권 - 몫 : %d, 나머지 : %d\n", money/curr50000, money%curr50000);
+        System.out.printf("50000원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr50000, money%curr50000);
         money %= curr50000;
-        System.out.printf("10000원 권 - 몫 : %d, 나머지 : %d\n", money/curr10000, money%curr10000);
+        System.out.printf("10000원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr10000, money%curr10000);
         money %= curr10000;
-        System.out.printf("5000원 권 - 몫 : %d, 나머지 : %d\n", money/curr5000, money%curr5000);
+        System.out.printf("5000원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr5000, money%curr5000);
         money %= curr5000;
-        System.out.printf("1000원 권 - 몫 : %d, 나머지 : %d\n", money/curr1000, money%curr1000);
+        System.out.printf("1000원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr1000, money%curr1000);
         money %= curr1000;
-        System.out.printf("500원 권 - 몫 : %d, 나머지 : %d\n", money/curr500, money%curr500);
+        System.out.printf("500원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr500, money%curr500);
         money %= curr500;
-        System.out.printf("100원 권 - 몫 : %d, 나머지 : %d\n", money/curr100, money%curr100);
+        System.out.printf("100원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr100, money%curr100);
         money %= curr100;
-        System.out.printf("50원 권 - 몫 : %d, 나머지 : %d\n", money/curr50, money%curr50);
+        System.out.printf("50원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr50, money%curr50);
         money %= curr50;
-        System.out.printf("10원 권 - 몫 : %d, 나머지 : %d\n", money/curr10, money%curr10);
+        System.out.printf("10원 권 - 몫 : %d 개, 나머지 : %d\n", money/curr10, money%curr10);
         money %= curr10;
     }
 }
