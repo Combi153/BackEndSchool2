ea4ecdfe05d52e4fbbf05a19d44945c508600cb9
chanminmaxscaler <jcm04@naver.com>
2022-10-04 16:39:38 +0900

    수업 커밋 코드업 홀수 짝수 문제 풀이 코드 수정

--- src/backend1004/evenodd2/Main.java
+++ src/backend1004/evenodd2/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 만약 첫번째 정수가 홀수이면 "홀수"를 출력하고, 짝수이면 "짝수"를 출력한 후  "+"를 출력한다.
 그리고 두번째 정수가 홀수이면 "홀수"를 출력하고, 짝수이면 "짝수"를 출력한 후  "="을 출력하고 결과로 나오는 값이 홀수인지 짝수인지 출력한다.
  */
+
 public class Main {
     private String getEvenOdd(int num) {
         if (num % 2 == 0){
@@ -26,6 +27,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int first = sc.nextInt();
         int second = sc.nextInt();
+
         Main main = new Main();
         main.printEvenOddStatement(first, second);
     }
