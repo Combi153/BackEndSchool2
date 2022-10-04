1685e685e6454c82d04f61c71d17b30eaa3f99cb
chanminmaxscaler <jcm04@naver.com>
2022-10-04 13:40:48 +0900

    수업 커밋 1. CoinTest : 금액을 화폐단위로 계산하기

---
+++ src/backend1004/CoinTest.java
@@ -0,0 +1,38 @@
+package backend1004;
+
+import java.util.Scanner;
+
+class Coin {
+    private int[] coinType = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+    private int[] coinTypeCnt;
+    private int cnt;
+    public Coin () {
+        coinTypeCnt = new int[coinType.length];
+    }
+
+    public void payCount(int money) {
+
+        for (int i = 0; i < coinType.length; i++){
+            cnt = 0;
+            if (money >= coinType[i]){
+                cnt = money / coinType[i];
+                money -= coinType[i] * cnt;
+                System.out.println(coinType[i] + "원 " + cnt + "개");
+            } else{
+                System.out.println(coinType[i] + "원 " + cnt + "개");
+            }
+        }
+    }
+}
+
+public class CoinTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Coin machine = new Coin();
+
+        System.out.print("금액을 넣으세요 : ");
+        int money = sc.nextInt();
+        machine.payCount(money);
+
+    }
+}
