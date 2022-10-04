5ad3d9a32522b722322255000350061c7b8a0324
chanminmaxscaler <jcm04@naver.com>
2022-10-04 14:26:26 +0900

    수업 커밋 나머지와 몫으로 풀이 단계 연산

--- src/backend1004/CurrencyTest.java
+++ src/backend1004/CurrencyTest.java
@@ -14,18 +14,11 @@ public class CurrencyTest {
         int curr500 = 500;
         int curr100 = 100;
 
-        System.out.println("5만원 권 : " + money/curr50000);
-        money %= curr50000;
-        System.out.println("1만원 권 : " + money/curr10000);
-        money %= curr10000;
-        System.out.println("5천원 권 : " + money/curr5000);
-        money %= curr5000;
-        System.out.println("1천원 권 : " + money/curr1000);
-        money %= curr1000;
-        System.out.println("500원 권 : " + money/curr500);
-        money %= curr500;
-        System.out.println("100원 권 : " + money/curr100);
-        money %= curr100;
-        System.out.println("남은 돈 : " + money);
+        System.out.printf("5만원 권 - 몫 : %d, 나머지 : %d\n", money/curr50000, money%curr50000);
+        System.out.printf("1만원 권 - 몫 : %d, 나머지 : %d\n", money/curr10000, money%curr10000);
+        System.out.printf("5000원 권 - 몫 : %d, 나머지 : %d\n", money/curr5000, money%curr5000);
+        System.out.printf("1000원 권 - 몫 : %d, 나머지 : %d\n", money/curr1000, money%curr1000);
+        System.out.printf("500원 권 - 몫 : %d, 나머지 : %d\n", money/curr500, money%curr500);
+        System.out.printf("100원 권 - 몫 : %d, 나머지 : %d\n", money/curr100, money%curr100);
     }
 }
