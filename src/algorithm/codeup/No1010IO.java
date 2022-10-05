851958414afb9b4b6ad15f8f352915f181b0f8f1
chanminmaxscaler <jcm04@naver.com>
2022-10-05 20:00:05 +0900

    알고리즘 과제 풀이 sc.close(); 추가

--- src/algorithm/codeup/No1010IO.java
+++ src/algorithm/codeup/No1010IO.java
@@ -2,10 +2,11 @@ package algorithm.codeup;
 
 import java.util.Scanner;
 
-public class No1010Print {
+public class No1010IO {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         System.out.println(num);
+        sc.close();
     }
 }
--- src/algorithm/codeup/No1011IO.java
+++ src/algorithm/codeup/No1011IO.java
@@ -2,10 +2,11 @@ package algorithm.codeup;
 
 import java.util.Scanner;
 
-public class No1011Print {
+public class No1011IO {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
         System.out.println(word);
+        sc.close();
     }
 }
