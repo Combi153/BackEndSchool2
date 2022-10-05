28e6d46dc74293da6c011c99e2f011d01888021e
chanminmaxscaler <jcm04@naver.com>
2022-10-05 10:48:27 +0900

    수업 커밋 Calculator2 실습 내용 생성자 추가, 멤버변수 이용

---
+++ src/date1005/javaexercise/Calculator2.java
@@ -0,0 +1,29 @@
+package date1005.javaexercise;
+
+public class Calculator2 {
+    private final int ERROR = -99999999;
+    private int a;
+    private int b;
+
+    public Calculator2(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(){
+        return a + b;
+    }
+    public int sub(){
+        return a - b;
+    }
+    public int mul(){
+        return a * b;
+    }
+    public double div(){
+        if(b == 0){
+            return ERROR;
+        } else{
+            return a / (double)b;
+        }
+    }
+}
---
+++ src/date1005/javaexercise/CalculatorMain2.java
@@ -0,0 +1,11 @@
+package date1005.javaexercise;
+
+public class CalculatorMain2 {
+    public static void main(String[] args) {
+        Calculator2 calc2 = new Calculator2(10, 20);
+        System.out.println(calc2.add());
+        System.out.println(calc2.sub());
+        System.out.println(calc2.mul());
+        System.out.println(calc2.div());
+    }
+}
