d1b29e0d6b42ab2edd11ef9f7597a684027aa922
chanminmaxscaler <jcm04@naver.com>
2022-10-06 10:09:43 +0900

    수업커밋 day3 List exercise 중복제거

--- src/date1006/collection/set/RndNumbersWithoutDuplicated.java
+++ src/date1006/collection/set/RndNumbersWithoutDuplicated.java
@@ -1,11 +1,20 @@
 package date1006.collection.set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RndNumbersWithoutDuplicated {
     public static void main(String[] args) {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        Set<Integer> randomNumberSet = new HashSet<>();
+
         for (int i = 0; i < 50; i++){
-            int num = randomNumberGenerator.generate(10);
-            System.out.println(num);
+            randomNumberSet.add(randomNumberGenerator.generate(10));
         }
+        for (Integer integer : randomNumberSet) {
+            System.out.println(integer);
+        }
+        System.out.println("집합 사이즈 : " + randomNumberSet.size());
+
     }
 }
