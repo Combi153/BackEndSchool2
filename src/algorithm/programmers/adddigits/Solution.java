ca63009f4063668dd67efee0e2e059e6c2dcf06f
chanminmaxscaler <jcm04@naver.com>
2022-10-12 09:06:15 +0900

    Add new algorithm solutions 프로그래머스 자릿수 구하기 - 배열 활용 - 수학 활용 - 나머지 연산 활용

--- src/algorithm/programmers/adddigits/Solution.java
+++ src/algorithm/programmers/adddigits/Solution.java
@@ -2,10 +2,11 @@ package algorithm.programmers.adddigits;
 
 public class Solution {
     public int solution(int n){
-        String strNumber = String.valueOf(n);
-        String[] strNumbers = strNumber.split("");
+        String strNumber = String.valueOf(n); //int n을 String으로 형변환
+        String[] strNumbers = strNumber.split(""); //String을 String[]으로 저장
         int answer = 0;
 
+        //반복문에서 배열의 요소를 int로 형변환하며 answer에 더하기
         for (int i = 0; i < strNumbers.length; i++){
             answer += Integer.parseInt(strNumbers[i]);
         }
--- src/algorithm/programmers/adddigits/Solution4.java
+++ src/algorithm/programmers/adddigits/Solution4.java
@@ -5,9 +5,9 @@ public class Solution4 {
 
         int answer = 0;
 
-        while (n > 0) {
-            answer += n % 10;
-            n = n / 10;
+        while (n > 0) { //n이 0 이하가 되면, 반복문을 나간다.
+            answer += n % 10; //answer에 n을 10으로 나눈 나머지를 더한다.
+            n /= 10; //n을 10으로 나눈 몫을 다시 n에 대입한다.
         }
         return answer;
     }
