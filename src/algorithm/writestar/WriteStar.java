ce202de91f78d254c58ba7d1adc5ac1bfad2ab14
chanminmaxscaler <jcm04@naver.com>
2022-10-17 10:18:31 +0900

    마름모 별 찍기

--- src/algorithm/writestar/WriteStar.java
+++ src/algorithm/writestar/WriteStar.java
@@ -1,16 +1,29 @@
 package algorithm.writestar;
 
+import java.util.Scanner;
+
 public class WriteStar {
     String letter;
+    int length;
+    Scanner sc;
 
-    public WriteStar() {
-        this.letter = "*";
-    }
-    public WriteStar(String letter) {
+    public WriteStar() {}
+
+    public WriteStar(String letter, int length) {
         this.letter = letter;
+        this.length = length;
     }
 
-    public void writeRightTriangle(int length){
+    public void setLetterAndLength() {
+        this.sc = new Scanner(System.in);
+        System.out.println("무엇을 찍을까요?");
+        this.letter = sc.next();
+        System.out.println("몇 줄을 찍을까요?");
+        this.length = sc.nextInt();
+        sc.close();
+    }
+
+    public void writeRightTriangle(){
 
         for (int i = 0; i < length + 1; i++) {
             for (int j = 0; j < i; j++) {
@@ -21,7 +34,7 @@ public class WriteStar {
         }
     }
 
-    public void writePyramid(int length){
+    public void writePyramid(){
 
         for (int i = 1; i < length + 1; i++) {
             for (int j = length ; j > i ; j--){
@@ -30,6 +43,30 @@ public class WriteStar {
             for (int j = 0; j < (i * 2) - 1; j++) {
                 System.out.print(letter);
 
+            }
+            System.out.println();
+        }
+    }
+
+    public void writeDiamond() {
+        for (int i = 1; i < length + 1; i++) {
+            for (int j = length ; j > i ; j--){
+                System.out.print("_");
+            }
+
+            for (int j = 0; j < (i * 2) - 1; j++) {
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+
+        for (int i = length - 1; i > 0; i--) {
+            for (int j = length ; j > i ; j--){
+                System.out.print("_");
+            }
+
+            for (int j = 0; j < (i * 2) - 1; j++) {
+                System.out.print(letter);
             }
             System.out.println();
         }
--- src/algorithm/writestar/WriteStarMain.java
+++ src/algorithm/writestar/WriteStarMain.java
@@ -4,12 +4,8 @@ import java.util.Scanner;
 
 public class WriteStarMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("무엇을 찍을까요?");
-        String letter = sc.next();
-        WriteStar letterWriter = new WriteStar(letter);
-        System.out.println("몇 줄을 찍을까요?");
-        int length = sc.nextInt();
-        letterWriter.writePyramid(length);
+        WriteStar letterWriter = new WriteStar();
+        letterWriter.setLetterAndLength();
+        letterWriter.writeDiamond();
     }
 }
