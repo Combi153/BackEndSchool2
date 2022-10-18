d6a472b73c7dd9d31dfb5bd9eb786d614ae0ee0b
chanminmaxscaler <jcm04@naver.com>
2022-10-18 09:16:46 +0900

    정사각형, 직사각형 별찍기

--- src/algorithm/writestar2/DrawStar.java
+++ src/algorithm/writestar2/DrawStar.java
@@ -2,9 +2,18 @@ package algorithm.writestar2;
 
 public class DrawStar {
 
-    public void square(int n) {
+    public void printSquare(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printRectangle(int x, int y) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
                 System.out.print("*");
             }
             System.out.println();
--- src/algorithm/writestar2/DrawStarMain.java
+++ src/algorithm/writestar2/DrawStarMain.java
@@ -6,8 +6,16 @@ public class DrawStarMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         DrawStar drawStar = new DrawStar();
-        int n = sc.nextInt();
 
-        drawStar.square(n);
+        System.out.println("정사각형 한 변의 길이를 입력하세요");
+        int n = sc.nextInt();
+        drawStar.printSquare(n);
+
+        System.out.println("직사각형의 높이를 입력하세요");
+        int height = sc.nextInt();
+        System.out.println("직사각형의 너비를 입력하세요");
+        int width = sc.nextInt();
+
+        drawStar.printRectangle(height, width);
     }
 }
